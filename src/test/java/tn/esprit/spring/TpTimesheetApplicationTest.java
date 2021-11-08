849e4cbd451107b681f57c4ee24cdf54d32dacef
azizcharfeddine <57187596+azizcharfeddine@users.noreply.github.com>
2021-11-08 03:44:40 +0100

    aziz

--- src/test/java/tn/esprit/spring/TpTimesheetApplicationTest.java
+++ src/test/java/tn/esprit/spring/TpTimesheetApplicationTest.java
@@ -101,7 +101,6 @@ private	IEntrepriseService entrepriseservice;
 	 		}
 	 		catch (Exception e) {l.error("Erreur dans testDeleteEmployeById() : " + e);}
 	          }
-	   //aziz
 	   @Autowired
 		@Test
 	   public void testAffecterEmployeADepartement() {
